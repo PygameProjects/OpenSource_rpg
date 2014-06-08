fda839845b41f6ff854dbc6451dbe26afe048107
MihaiPro <anonimatanonimus@gmail.com>
2014-06-08 12:33:46 +0300

    Update Account.py

--- Bank/Account.py
+++ Bank/Account.py
@@ -1,7 +1,8 @@
 class Account(object):
   """ Allow to user to have a personal account 
   and keep their money here, to buy , sell and borrow
-  proprieties -> dictionary with name of propriety and its value"""
+  proprieties -> dictionary with name of propriety and its value
+  ststus -> allowed, banned, waiting(must be accepted by admin im max 3 days)"""
   money = 0
   total_debt = 0
   pay_per_month = 0
@@ -12,10 +13,11 @@ class Account(object):
   income = 0
   taxes = 0
   
-  def __init__(self, username, password, credit_card):
+  def __init__(self, username, password, credit_card, status):
     self.username = username
     self.password = password
     self.credit_card = credit_card
+    self.status = status
     
   def buy(self, thing, value):
     #add a thing to proprieties and pay for it
@@ -35,4 +37,8 @@ class Account(object):
   
   def deposit_money(self, amount):
     #deposit an amount of money in bank
+    pass
     
+  def borrow(self, amount, guarantee):
+    #borrow money, but user must have a guarantee(proprieties)
+    pass
