590c5283b7735e846986ef2f3b578988d1cb5dc4
MihaiPro <anonimatanonimus@gmail.com>
2014-06-08 12:44:26 +0300

    Update db.py

---
+++ Bank/db.py
@@ -0,0 +1,21 @@
+class DB(object):
+  """This keep all users"""
+  total_money = 0
+  users = []
+  def __init__(self, bank_name, password):
+    self.bank_name = bank_name
+    self.password = password
+    
+  def addUser(self, user):
+    #add user to DB
+    pass
+  
+  def removeUser(self, user):
+    #remove user from DB
+    pass
+  
+  def changeAttribute(self, user, attribute, value):
+    #access one attribute of a user and change his value
+    pass
+  
+database = DB('test', 'test')
