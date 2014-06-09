608ca254aef940a0f8ec03d48f4afdaf3ec9d926
MihaiPro <anonimatanonimus@gmail.com>
2014-06-09 08:41:09 +0300

    Update main.py

--- Bank/main.py
+++ Bank/main.py
@@ -21,12 +21,29 @@ def register():
       f.write('\n')
       f.write('database.addUser('+user+')')
       f.close()
+    else: print "Username already exist!"
+  else: print "There are empty boxes!"
 
 def login():
-  pass
+  print "Log in!"
+  user = raw_input("User: ")
+  password = raw_input("Password: ")
+  if user in database.users.keys():
+    if database.users[user].password = password:
+      print "You logged in!"
+    print "Wrong password!"
+  print "Username doesn't exist!"
 	
 	
 def main():
-  pass
+  print "Welcome to " + TheBank.name + " bank!"
+  print "Type 'log in' to enter in your account, or 'sign up' to register."
+  c = raw_input("> ")
+  if c == 'log in':
+    login()
+  elif c == 'sign up':
+    register()
+  else:
+    print "This command doesn't exist!"
 	
-if __name__ == '__main__': print database.users['Mihai'].money
+if __name__ == '__main__': main()
